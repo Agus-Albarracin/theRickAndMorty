44d03753d9a7dc64c288899d0fbe491cc57edbd8
Agus-Albarracin <abusscin_@hotmail.com>
2023-09-18 12:27:53 -0300

    tarea 06, card, cards, searchBar

---
+++ theRickandMorty/src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+const Card = ({name, status, species, gender, origin, image, onClose}) => {
+   return (
+      <div>
+          <button onClick={onClose}>X</button>
+          <h2>{name};</h2>
+          <h2>{status}</h2>
+          <h2>{species}</h2>
+          <h2>{gender}</h2>
+          <h2>o{origin?.name}</h2>
+          <img src={image} alt={name} /> 
+      </div>
+   );
+}
+
+export default Card;
