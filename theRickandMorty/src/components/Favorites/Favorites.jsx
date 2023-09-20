b5fa4167b9ad311ef3e5f9deabb17c318150a48c
Agus-Albarracin <abusscin_@hotmail.com>
2023-09-20 12:39:05 -0300

    tarea 13 - Se agrego actions en el reducer (filter y order) | se agrego etiquetas select en componente Fav. |

--- theRickandMorty/src/components/Favorites/Favorites.jsx
+++ theRickandMorty/src/components/Favorites/Favorites.jsx
@@ -1,11 +1,39 @@
-import { connect } from "react-redux"
 import  Card from '../Card/Card'
+
+import { filterCards, orderCards } from '../../redux/actions'
+
+import { useDispatch } from "react-redux"
+import { connect } from "react-redux"
+import { useState } from 'react'
 
 
 const Favorites = ({myFavorites}) =>{
+const dispatch = useDispatch();
+const [aux, setAux] = useState(false)
 
+const handleOrder = (event) =>{
+    dispatch(orderCards(event.target.value))
+    setAux(!aux)
+}
+
+const handleFilter = (event) =>{
+    dispatch(filterCards(event.target.value))
+}
     return(
         <div>
+            <select onChange={handleOrder}>
+            <option value="A">Ascendente</option>
+            <option value="D">Descendente</option>
+            </select>
+
+            <select onChange={handleFilter}>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+            <option value="Genderless">Genderless</option>
+            <option value="unknown">unknown</option>
+            <option value="All">Todos</option>
+
+            </select>
             {
             myFavorites.map(fav =>{
                 return(
--- theRickandMorty/src/redux/reducer.js
+++ theRickandMorty/src/redux/reducer.js
@@ -1,24 +1,46 @@
-import { ADD_FAV, REMOVE_FAV } from "./actions"
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER} from "./actions"
 
 const initialState = {
-    myFavorites: []
+    myFavorites: [],
+    allCharacters: []
 }
 
 const reducer = (state = initialState, action) => {
     switch(action.type){
 
     case "ADD_FAV":
-    return {...state, myFavorites: [...state.myFavorites, action.payload]}
+    return {...state,
+        myFavorites: [...state.allCharacters, action.payload],
+        allCharacters: [...state.allCharacters, action.payload]}
 
     case "REMOVE_FAV":
     return {...state,
             myFavorites: state.myFavorites.filter(
                 (char) => char.id !== action.payload)}
+    
+    case "FILTER":
+    if (action.payload === "All")
+    return {...state,myFavorites:state.allCharacters};
 
+    const charFilter = state.allCharacters.filter((char) => char.gender === action.payload)
+    return {...state, myFavorites: charFilter}
 
-                    default:
-                        return {...state}
-                }
+    case "ORDER":
+    const copyAllChar = [...state.allCharacters];
+    return {
+        ...state,
+        myFavorites:
+        action.payload === "A"
+        ? copyAllChar.sort((a,b) => a.id - b.id)
+        : copyAllChar.sort((a,b) => b.id - a.id)
+    }
+
+    
+
+    default:
+    return {...state}
+    }
+        
 }
 
 export default reducer;
