256f0e1293b4af2c5c791b8c9be38c2a7cf9143c
Agus-Albarracin <abusscin_@hotmail.com>
2023-10-02 16:11:09 -0300

    Server creado, puerto en archivo INDEX, prueba con json data en carpeta utils. Se modifico la url en Client de OnSearch y detail

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
    }
 
    const onSearch = (id) => {
-      axios(`https://rickandmortyapi.com/api/character/${id}`)
+      axios(`http://localhost:3001/rickandmorty/character/${id}`)
       .then(({ data }) => {
          if (data.name) {
 
